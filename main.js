4e39a282e5a065c0162b4918f4b554ce3ba67dc6
Vlastimil Slechta <102651272+valCech@users.noreply.github.com>
2022-08-12 12:29:33 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,29 +1,51 @@
-//alert('Welcome to Val\'s project');
+alert('Welcome to Val\'s project');
 var fname = document.getElementById("fname").value;
-var at = document.getElementById("name").value;
 var age = document.getElementById("age").value;
-var fname = document.getElementById("height").value;
-var fname = document.getElementById("height").value;
-submitOK = "true";
+var height = document.getElementById("height").value;
+var weight = document.getElementById("weight").value;
+var gender = document.querySelector("#gender").value
 
-if (fname == "") {  
-    alert("Please enter your first name");
-    submitOK = "false";
+// Form validation(all fileds must be filled)
+function validateForm() {
+  let name = document.forms["myForm"]["name"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+  let age = document.forms["myForm"]["age"].value;
+  if (x == "") {
+    alert("Age must be filled out");
+    return false;
+  }
+  let weight = document.forms["myForm"]["weight"].value;
+  if (x == "") {
+    alert("Weight must be filled out");
+    return false;
+  }
+  let height = document.forms["myForm"]["height"].value;
+  if (x == "") {
+    alert("Height must be filled out");
+    return false;
+  }
+} 
 
-if (isNaN(age) || age < 1 || age > 100) {
-  alert("The age must be a number between 1 and 100");
-  submitOK = "false";
-}
+// function dropList() {
+//   var mylist = document.getElementById("myList");
+//   document.getElementById("gender").value = mylist.options[mylist.selectedIndex].text;}
 
-if (at == -1) {
-  // alert("Not a valid e-mail!");
-  submitOK = "false";
-}
-
-if (submitOK == "false") {
-  alert("Please fix the above errors and try again");
-}}
-
-function dropList() {
-  var mylist = document.getElementById("myList");
-  document.getElementById("gender").value = mylist.options[mylist.selectedIndex].text;}
+// // BMI (Body Mass Index)
+// var bmi = weight / ((height/100)**2);
+// BMI1 = round(BMI, 1)
+//   alert("Your BMI is " + BMI1);
+// if (BMI1 < 18.5) {
+//   alert("You are underweight");
+// }
+// else if (BMI1 >= 18.5 && BMI1 < 25) {
+//   alert("You are normal");
+// } 
+// else if (BMI1 >= 25 && BMI1 < 30) {
+//   alert("You are overweight");
+// }
+// else if (BMI1 >= 30) {
+//   alert("You are obese");
+// }}
